0897ea75285ec4b3eeb3cf0c716981a8e7cd1389
Neetu Chordia <chordianeetu@gmail.com>
2022-08-08 14:40:01 -0700

    Update TodoContainer.js

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,140 +1,122 @@
-//this is complete
+import React, { useState, useContext } from "react";
+import ModeEditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import { useDispatch, useSelector } from "react-redux";
+import { Remove, Update_data } from "../redux/actions/action";
+import { DeleteContext } from "./context/ContextProvider";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-import React, { Component } from "react"
-import TodoList from "./TodoList"
-import Header from "./Header"
-import InputTodo from "./InputTodo"
-import { v4 as uuidv4 } from "uuid";
+const TodoList = () => {
+    const { User_data } = useSelector((state) => state.todoreducers);
+    // console.log(User_data);
+    const { dlttask, setDlettask } = useContext(DeleteContext);
 
+    const dispatch = useDispatch();
+    //show modal state
+    const [showeye, setShoweye] = useState(false);
+    //set value state
+    const [showeyevalue, setShoweyeValue] = useState("");
+    //state to  open modal
+    const [show, setShow] = useState(false);
 
-class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state={
-			items: [],
-			itemsToShow: "all",
-			id: uuidv4(),
-			item: '',
-			editItem: false,
-		}
-	}
+    const [update, setUpdate] = useState("");
 
-	handleChange = event => {
-		this.setState({
-			item: event.target.value
-		})
-	}
+    const [ind, setInd] = useState("");
 
-	handleSubmit = event => {
-		event.preventDefault()
-		
-		const newItem = {
-			id: this.state.id,
-			title: this.state.item,
-			completed: false
-		}
-		
-		const updatedItems = [...this.state.items, newItem]
+    const handleClose = () => setShow(false);
 
-		if (this.state.item.length > 0) {
-			this.setState({
-				items: updatedItems,
-				id: uuidv4(),
-				item: '',
-				editItem: false
-			})
-		}
-	}
+    const handelShow = (el) => {
+        setShow(true);
+        setUpdate(el);
+    };
 
-	updateTodosToShow = string => {
-		this.setState({
-			itemsToShow: string
-		});
-	};
+    //delete function
+    const remove = (id) => {
+        dispatch(Remove(id));
+        setDlettask(true);
+    };
+    // update function
+    const usertask_update = () => {
+        dispatch(Update_data(update, ind));
+        handleClose();
+    };
+    return (
+        <>
+            <div className="todo_data col-lg-5 mx-auto mt-2">
+                { 
+                    User_data.map((ele, k) => {
+                    return (
+                        <>
+                            <div
+                                className="todo_container mb-2 d-flex justify-content-between align-items-center px-2"
+                                key={k}
+                                style={{
+                                    background: "#dcdde1",
+                                    borderRadius: "3px",
+                                    height: "45px",
+                                }}
+                            >
+                                <li style={{ listStyle: "none" }}>{ele}</li>
+                                <div className="edit_dlt col-lg-3 py-2 d-flex justify-content-between align-items-center">
+                                    <ModeEditIcon
+                                        onClick={() => {
+                                            handelShow(ele);
+                                            setInd(k);
+                                        }}
+                                        style={{ color: "#3c40c6", cursor: "pointer" }}
+                                    />
+                                    <DeleteIcon
+                                        onClick={() => remove(k)}
+                                        style={{ color: "red", cursor: "pointer" }}
+                                    />
+                                    <RemoveRedEyeIcon
+                                        onClick={() => {
+                                            setShoweye(true);
+                                            setShoweyeValue(ele);
+                                        }}
+                                        style={{ color: "#1dd1a1", cursor: "pointer" }}
+                                    />
+                                </div>
+                            </div>
+                        </>
+                    );
+                })}
 
-	handleDoneTask = (id, completed) => {
-		const filteredItems = this.state.items.map(item => {
-			item.id === id && (item.completed = !item.completed)
-			return item
-		})
+                {/* read model */}
+                <Modal show={showeye}>
+                    <h1 className='text-center'>{showeyevalue}</h1>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => setShoweye(false)}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
 
-		this.setState({
-			items: filteredItems,
-		})
-	}
+                {/* update modal */}
 
-	handleDelete = id => {
-		const filteredItems = this.state.items.filter(item => item.id !== id)
+                <Modal show={show} onHide={handleClose}>
+                <h3 className='text-center mt-2'>Update Your Task</h3>
+                    <Modal.Header>
+                    <div className="todo col-lg-5 mx-auto d-flex justify-content-between align-items-center">
+                        <input name='task' value={update}
 
-		this.setState({
-			items: filteredItems
-		})
-	}
+                         onChange={(e)=>setUpdate(e.target.value)} className='form-control col-lg-5 mt-2' />
+                    </div>
 
-	handleEdit = id => {
-		const filteredItems = this.state.items.filter(item => item.id !== id)
-
-		const selectedItem = this.state.items.find(item => item.id === id)
-
-		this.setState({
-			items: filteredItems,
-			id: id,
-			item: selectedItem.title,
-			editItem: true
-		})
-	}
-
-	handleDeleteDoneTasks = () => {
-		const filteredItems = this.state.items.filter(item => item.completed === false)
-
-		this.setState({
-			items: filteredItems
-		})
-	}
-
-	clearList = () => {
-		this.setState({
-			items: []
-		})
-	}
-
-	render() {
-		let items = []
-
-		if (this.state.itemsToShow === "all") {
-			items = this.state.items;
-		} else if (this.state.itemsToShow === "todo") {
-			items = this.state.items.filter(item => !item.completed);
-		} else if (this.state.itemsToShow === "done") {
-			items = this.state.items.filter(item => item.completed);			
-		}
-
-		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-60 col-md-50 mx-auto mt-10">
-            <Header />
-						<h3 className="text-capitalize text-center">TodoInput</h3>
-						<InputTodo
-							item={this.state.item}
-							handleChange={this.handleChange}
-							handleSubmit={this.handleSubmit}
-						/>
-						<TodoList
-							items={items}
-							filterDoneTasks={this.filterDoneTasks}
-							clearList={this.clearList}
-							handleDelete={this.handleDelete}
-							handleEdit={this.handleEdit}
-							handleDoneTask={this.handleDoneTask}
-							handleDeleteDoneTasks={this.handleDeleteDoneTasks}
-							updateTodosToShow={this.updateTodosToShow}
-						/>
-					</div>
-				</div>
-			</div>
-		);
-	}
-}
-
-export default App;
+                    </Modal.Header>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={()=>usertask_update()}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        </>
+    );
+};
+export default TodoList;
