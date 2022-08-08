5d3f4de28162cf29ff6e2abee45fd9e163eb6807
Neetu Chordia <chordianeetu@gmail.com>
2022-08-08 14:39:23 -0700

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,97 +1,122 @@
-//this is complete
-import React, { Component, Fragment } from 'react'
-import TodoItem from './TodoItem'
+import React, { useState, useContext } from "react";
+import ModeEditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import { useDispatch, useSelector } from "react-redux";
+import { Remove, Update_data } from "../redux/actions/action";
+import { DeleteContext } from "./context/ContextProvider";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-export default class TodoList extends Component {
-    render() {
-        const {
-            items,
-            updateTodosToShow,
-            clearList,
-            handleDelete,
-            handleEdit,
-            handleDoneTask,
-            handleDeleteDoneTasks
-        } = this.props
+const TodoList = () => {
+    const { User_data } = useSelector((state) => state.todoreducers);
+    // console.log(User_data);
+    const { dlttask, setDlettask } = useContext(DeleteContext);
 
-        return (
-            <Fragment>
-                <h3 className="text-center">
-                    TodoList
-                </h3>
+    const dispatch = useDispatch();
+    //show modal state
+    const [showeye, setShoweye] = useState(false);
+    //set value state
+    const [showeyevalue, setShoweyeValue] = useState("");
+    //state to  open modal
+    const [show, setShow] = useState(false);
 
-                <div className="row">
-                    <div className="col-md-4">
-                        <button 
-                            type="button"
-                            className="btn btn-info btn-block mt-1"
-                            onClick={() => updateTodosToShow("all")}
-                        >
-                            All
-                        </button>
-                    </div>
-                    <div className="col-md-4">
-                        <button 
-                            type="button"
-                            className="btn btn-info btn-block mt-1"
-                            onClick={() => updateTodosToShow("done")}
-                        >
-                            Done
-                        </button>
-                    </div>
-                    <div className="col-md-4">
-                        <button 
-                            type="button"
-                            className="btn btn-info btn-block mt-1"
-                            onClick={() => updateTodosToShow("todo")}
-                        >
-                            Todo
-                        </button>
-                    </div>
-                </div>
+    const [update, setUpdate] = useState("");
 
-                {
-                items.length === 0 ? '' :
-                    <ul className="list-group my-5">
-                        {
-                            items.map(item => {
-                                return (
-                                    <TodoItem
-                                        key={item.id}
-                                        id={item.id}
-                                        title={item.title}
-                                        completed={item.completed}
-                                        handleDelete={() => handleDelete(item.id)}
-                                        handleEdit={() => handleEdit(item.id)}
-                                        handleDoneTask={handleDoneTask}
+    const [ind, setInd] = useState("");
+
+    const handleClose = () => setShow(false);
+
+    const handelShow = (el) => {
+        setShow(true);
+        setUpdate(el);
+    };
+
+    //delete function
+    const remove = (id) => {
+        dispatch(Remove(id));
+        setDlettask(true);
+    };
+    // update function
+    const usertask_update = () => {
+        dispatch(Update_data(update, ind));
+        handleClose();
+    };
+    return (
+        <>
+            <div className="todo_data col-lg-5 mx-auto mt-2">
+                { 
+                    User_data.map((ele, k) => {
+                    return (
+                        <>
+                            <div
+                                className="todo_container mb-2 d-flex justify-content-between align-items-center px-2"
+                                key={k}
+                                style={{
+                                    background: "#dcdde1",
+                                    borderRadius: "3px",
+                                    height: "45px",
+                                }}
+                            >
+                                <li style={{ listStyle: "none" }}>{ele}</li>
+                                <div className="edit_dlt col-lg-3 py-2 d-flex justify-content-between align-items-center">
+                                    <ModeEditIcon
+                                        onClick={() => {
+                                            handelShow(ele);
+                                            setInd(k);
+                                        }}
+                                        style={{ color: "#3c40c6", cursor: "pointer" }}
                                     />
-                                )
-                            })
-                        }
+                                    <DeleteIcon
+                                        onClick={() => remove(k)}
+                                        style={{ color: "red", cursor: "pointer" }}
+                                    />
+                                    <RemoveRedEyeIcon
+                                        onClick={() => {
+                                            setShoweye(true);
+                                            setShoweyeValue(ele);
+                                        }}
+                                        style={{ color: "#1dd1a1", cursor: "pointer" }}
+                                    />
+                                </div>
+                            </div>
+                        </>
+                    );
+                })}
 
-                        <div className="row mt-4">
-                            <div className="col-md-6">
-                                <button 
-                                    type="button"
-                                    className="btn btn-danger btn-block mt-1"
-                                    onClick={handleDeleteDoneTasks}
-                                >
-                                    Delete done tasks
-                                </button>
-                            </div>
-                            <div className="col-md-6">
-                                <button 
-                                    type="button"
-                                    className="btn btn-danger btn-block mt-1"
-                                    onClick={clearList}
-                                >
-                                    Delete all tasks
-                                </button>
-                            </div>
-                        </div>
-                    </ul>
-                }
-            </Fragment>
-        )
-    }
-}
+                {/* read model */}
+                <Modal show={showeye}>
+                    <h1 className='text-center'>{showeyevalue}</h1>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => setShoweye(false)}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                {/* update modal */}
+
+                <Modal show={show} onHide={handleClose}>
+                <h3 className='text-center mt-2'>Update Your Task</h3>
+                    <Modal.Header>
+                    <div className="todo col-lg-5 mx-auto d-flex justify-content-between align-items-center">
+                        <input name='task' value={update}
+
+                         onChange={(e)=>setUpdate(e.target.value)} className='form-control col-lg-5 mt-2' />
+                    </div>
+
+                    </Modal.Header>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={()=>usertask_update()}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        </>
+    );
+};
+export default TodoList;
